93bd71f35eb6189c14de5ae6d8e4463c86dafdb3
Caleb Leavell <calebjleavell06@gmail.com>
2025-10-16 16:13:32 -0700

    implement species

---
+++ species.py
@@ -0,0 +1,16 @@
+class species:
+    def __init__(self, attribute_names: list[str]):
+        self.attributes = []
+        for name in attribute_names:
+            self.attributes.append({
+                    "name": name,
+                    "attribute": ""
+                })
+            
+    def set_attribute(self, name: str, attribute: str):
+        for attr in self.attributes:
+            if attr["name"] == name:
+                attr["attribute"] = attribute
+
+
+    
